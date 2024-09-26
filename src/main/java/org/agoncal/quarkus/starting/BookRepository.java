20faaf3bb3f3c19f559838950acde9e50836404c
hocinefdn <hocinefedani@gmail.com>
2024-09-26 13:14:55 +0100

    end of project

--- src/main/java/org/agoncal/quarkus/starting/BookRepository.java
+++ src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -2,6 +2,7 @@ package org.agoncal.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.PathParam;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,12 +10,14 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "books.genre",defaultValue = "Sci-Fi")
+    String genre;
 
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "Understanding Quarkus", "Antonio", 2020, "IT"),
-                new Book(2, "Java", "Antonio", 2020, "IT"),
-                new Book(3, "Thinking Java", "Antonio", 1998, "IT")
+                new Book(1, "Understanding Quarkus", "Antonio", 2020, genre),
+                new Book(2, "Java", "Antonio", 2020, genre),
+                new Book(3, "Thinking Java", "Antonio", 1998, genre)
         );
     }
 
